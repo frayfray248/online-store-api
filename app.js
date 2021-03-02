9fb78ae386ed6cb947eddee102b62b6d99847a90
frayfray248 <nicman248@hotmail.com>
2021-03-02 15:06:04 -0800

    serve documentation

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 //imports
 const express = require('express');
 const http = require('http');
+const swagger = require('swagger-ui-express');
+const YAML = require('yamljs');
 
 // port
 const PORT = process.env.PORT || 3000;
 
 // instances
+const OAS = YAML.load('./online-store-api-doc.yml');
 const app = express();
 const server =  http.createServer(app);
+
+// documentation
+app.use('/api-docs', swagger.serve, swagger.setup(OAS));
 
 // routes
 app.use('/', (req, res, next) => res.send('Hello World').status(200));
