ee46a58d452b9302eee4bf89e3b3e8220bae2fe6
frayfray248 <nicman248@hotmail.com>
2021-03-03 11:36:46 -0800

    product model

---
+++ api/models/product.js
@@ -0,0 +1,27 @@
+// db
+const mongoose = require('mongoose');
+
+// model
+const ProductSchema = mongoose.Schema({
+    name : {
+        type : String,
+        required: true,
+        minLength: 3,
+        maxLength: 20
+    },
+    description : {
+        type: String,
+        minLength: 3,
+        maxLength: 255
+    },
+    price : {
+        type: Number
+    },
+    image : {
+        data: Buffer,
+        contentType: String
+    }
+});
+
+// export
+module.exports = mongoose.model('Product', ProductSchema);
