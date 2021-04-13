2e495b9fb77c10015633450c1a7fc04ec44a520e
frayfray248 <nicman248@hotmail.com>
2021-04-13 13:40:05 -0700

    saving file storage and limits

--- api/routes/ProductsRoute.js
+++ api/routes/ProductsRoute.js
@@ -3,8 +3,32 @@ const express = require('express');
 const multer = require('multer');
 const ProductsController = require('../controllers/ProductsController');
 
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './uploads/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + file.originalname)
+    }
+});
+
+const fileFilter = (req, file, cb) => {
+    // reject a file
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+        cb(null, false);
+    } else {
+        cb(null, true);
+    }
+};
+
 // multer upload
-const upload = multer({ dest: 'uploads/' });
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 1024 * 1024 * 5
+    },
+    fileFilter: fileFilter
+});
 
 // router
 const router = express.Router();
