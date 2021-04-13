129fc760b13496a863c56a8d3c79609bfb044d0d
frayfray248 <nicman248@hotmail.com>
2021-04-13 14:14:52 -0700

    upload middleware

---
+++ api/middleware/upload.js
@@ -0,0 +1,32 @@
+// imports
+const multer = require('multer');
+
+// storage
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, './uploads/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + file.originalname)
+    }
+});
+
+// file filter
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+        cb(null, true);
+    } else {
+        cb(new Error("Invalid File Type"), false);
+    }
+};
+
+// upload
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 1024 * 1024 * 5
+    },
+    fileFilter: fileFilter
+});
+
+module.exports = upload;
